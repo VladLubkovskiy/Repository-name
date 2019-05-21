c64875efe20c2197fd34bbc008d7ea6379b666c1
VladLubkovskiy <vladlubkovskiy1@gmail.com>
2019-05-21 19:01:30 +0300

    commit

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -17,9 +17,11 @@ namespace ConsoleApp18
             GraphicsWindow.MouseMove += () => { Shapes.Move(p, GraphicsWindow.MouseX-50, GraphicsWindow.Height-20); };
             var c = Shapes.AddEllipse(20, 20);
             Timer.Interval = 10;
-            int x = 0, y = 0,stepx=2,stepy=2;
+            int x = 0, y = 0,stepx=2,stepy=2, my = GraphicsWindow.Height - 40,mx = GraphicsWindow.MouseX;
             Timer.Tick += () => 
             {
+                my = GraphicsWindow.Height - 40;
+                mx = GraphicsWindow.MouseX;
                 Shapes.Move(c, x+=stepx, y+=stepy);
                 if (x >= GraphicsWindow.Width-20)
                 {
@@ -29,13 +31,18 @@ namespace ConsoleApp18
                 {
                     stepx = 2;
                 }
-                if ((x>=GraphicsWindow.Width-50||x<=GraphicsWindow.Width+50)&&(y>=GraphicsWindow.Height-10))
+                if ((x >= mx - 50 && x <= mx + 50) && (y >= my))
                 {
                     stepy = -2;
                 }
                 if (y <= 0)
                 {
                     stepy = 2;
+                }
+                if(y > GraphicsWindow.Height)
+                {
+                    Sound.PlayAndWait("1.mp3");
+                    Microsoft.SmallBasic.Library.Program.End();
                 }
             };
         }
