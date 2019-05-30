a2ccefff7049f3a1622aaa022b1d4469efe2b509
VladLubkovskiy <vladlubkovskiy1@gmail.com>
2019-05-30 19:00:28 +0300

    666

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -15,49 +15,57 @@ namespace ConsoleApp18
             var p = Shapes.AddRectangle(100, 20);
             Shapes.Move(p,0, GraphicsWindow.Height - 20);
             GraphicsWindow.MouseMove += () => { Shapes.Move(p, GraphicsWindow.MouseX-50, GraphicsWindow.Height-20); };
-            Primitive[] enemies = new Primitive[10];
+            Primitive[] enemies = new Primitive[20];
             GraphicsWindow.BrushColor = "Gray";
             var c = Shapes.AddEllipse(20, 20);
             Timer.Interval = 10;
-            int x = 0, y = 0,stepx=2,stepy=2, my = GraphicsWindow.Height - 40,mx = GraphicsWindow.MouseX;
+            int circleX = GraphicsWindow.Width/2, circleY = GraphicsWindow.Height/2,stepx=2,stepy=2, my = GraphicsWindow.Height - 40,mx = GraphicsWindow.MouseX;
+            int enemyWidth = GraphicsWindow.Width / 10 - 5;
             Mouse.HideCursor();
             GraphicsWindow.BrushColor = "Red";
             for(int i = 0; i < 10; i++)
             {
-                enemies[i] = Shapes.AddRectangle(GraphicsWindow.Width/10, 20);
-                Shapes.Move(enemies[i],i* (GraphicsWindow.Width / 10), 0);
-            }          
+                enemies[i] = Shapes.AddRectangle(enemyWidth, 20);
+                Shapes.Move(enemies[i],i* (GraphicsWindow.Width / 10), 30);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                enemies[i] = Shapes.AddRectangle(enemyWidth, 20);
+                Shapes.Move(enemies[i], i * (GraphicsWindow.Width / 10), 55);
+            }
+
             Timer.Tick += () => 
             {
                 my = GraphicsWindow.Height - 40;
                 mx = GraphicsWindow.MouseX;
-                Shapes.Move(c, x+=stepx, y+=stepy);
-                if (x >= GraphicsWindow.Width-20)
+                Shapes.Move(c, circleX+=stepx, circleY+=stepy);
+                if (circleX >= GraphicsWindow.Width-20)
                 {
-                    stepx = -4;
+                    stepx = -stepx;
                 }
-                if (x <= 0)
+                if (circleX <= 0)
                 {
                     stepx = 4;
                 }
-                if ((x >= mx - 50 && x <= mx + 50) && (y >= my))
+                if ((circleX >= mx - 50 && circleX <= mx + 50) && (circleY >= my))
                 {
-                    stepy = -4;
+                    stepy = -stepy;
                 }
-                if (y <= 0)
+                if (circleY <= 0)
                 {
                     stepy = 4;
                 }
-                if(y > GraphicsWindow.Height)
+                if(circleY > GraphicsWindow.Height)
                 {
                     Sound.PlayAndWait("1.mp3");
                     Microsoft.SmallBasic.Library.Program.End();
                 }
                 for (int i = 0; i < 10; i++)
                 {
-                    if ((x + 10 == Shapes.GetLeft(enemies[i])) && (y == Shapes.GetTop(enemies[i])))
+                    if ((circleX + 10 >= Shapes.GetLeft(enemies[i])) && (circleY <= Shapes.GetTop(enemies[i])+20)&&(circleX<=Shapes.GetLeft(enemies[i]) + enemyWidth))
                     {
-                        Shapes.HideShape(enemies[i]);
+                        Shapes.Move(enemies[i],9999,9999);
+                        stepy = -stepy;
                     }
                 }
             };
